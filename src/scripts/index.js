94ba83d8cbc17822bc89de5f32057c25755a2e67
Christopher Coleman <chris@RRA-Kali-PC.home>
2020-08-22 19:35:14 -0400

    Rock, Paper, Scissors buttons fully functional. App working. Just need to get the alert to print to the screen instead. I think I'll save that for jQuery tomorrow.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,55 +18,48 @@ const createChoiceButton = choice => {
   return newButton
 }
 
+// get random computer choice of rock, paper, or scissors based on percentage
+const computerPick = (() => {
+  const randomChoice = Math.random()
+  if (randomChoice < (1/3)) {
+    return "Rock"
+  } else if (randomChoice > (2/3)) {
+    return "Scissors"
+  } else {
+    return "Paper"
+  }
+})()
+
 /*
 * function that performs the rock paper scissors based on random computer choice
 */
-const rockPaperScissors = ((userChoice) => {
-  // get random computer choice of rock, paper, or scissors based on percentage
-  const computerChoice = (() => {
-    const randomChoice = Math.random()
-    if (randomChoice < (1/3)) {
-      return "Rock"
-    } else if (randomChoice > (2/3)) {
-      return "Scissors"
-    } else {
-      return "Paper"
-    }
-  })()
-  
+const rockPaperScissors = ((computerEntry, userEntry) => {
   // create function to display who is the winner between user prompt choice and random computer choice
-  const getWinner = (computerEntery, userEntery) => {
-    if (userEntery === computerEntery) {
-      return alert("Tie! Try Again!")
-    } 
-    if (userEntery === "Rock" && computerEntery === "Scissors") {
-      return alert(winnerString)
-    }
-    if (userEntery === "rock" && computerEntery === "paper") {
-      return alert(looserString)
-    }
-    if (userEntery === "paper" && computerEntery === "rock") {
-      return alert(winnerString)
-    }
-    if (userEntery === "paper" && computerEntery === "scissors") {
-      return alert(looserString)
-    }
-    if (userEntery === "scissors" && computerEntery === "paper") {
-      return alert(winnerString)
-    }
-    if (userEntery === "scissors" && computerEntery === "rock") {
-      return alert(looserString)
-    }if (userEntery === "nuke") { // always win ;)
-      return alert(winnerString)
-    } else { // error handling
-      return alert("Invalid Entery. Please choose Rock, Paper, or Scissors.")
-    }
+  if (userEntry === computerEntry) {
+    return alert("Tie! Try Again!")
+  } 
+  if (userEntry === "Rock" && computerEntry === "Scissors") {
+    return alert(winnerString)
   }
-  // call the getWinner function and pass in the usrchoice and compchoice as parameters
-  getWinner(computerChoice, userChoice)
-  // log the choices to make sure the program is running correctly
-  console.log("userChoice => " + userChoice)
-  console.log("computerChoice => " + computerChoice)
+  if (userEntry === "Rock" && computerEntry === "Paper") {
+    return alert(looserString)
+  }
+  if (userEntry === "Paper" && computerEntry === "Rock") {
+    return alert(winnerString)
+  }
+  if (userEntry === "Paper" && computerEntry === "scissors") {
+    return alert(looserString)
+  }
+  if (userEntry === "scissors" && computerEntry === "Paper") {
+    return alert(winnerString)
+  }
+  if (userEntry === "scissors" && computerEntry === "Rock") {
+    return alert(looserString)
+  }if (userEntry === "Nuke") { // always win ;)
+    return alert(winnerString)
+  } else { // error handling
+    return alert("Invalid Entery. Please choose Rock, Paper, or Scissors.")
+  }
 })
 
 /*
@@ -83,6 +76,8 @@ let choiceButtonArray = Array.from(document.getElementsByClassName("choiceButton
 // and userChoice as the button clicked
 choiceButtonArray.forEach(e => {
   e.addEventListener(('click'), (e => {
-    rockPaperScissors(e.target.textContent)
+    let userPick = e.target.textContent
+    console.log(userPick, computerPick)
+    rockPaperScissors(userPick, computerPick)
   }))
 })
